06e19169c496fa6e5df020496f8a51739294bc80
Makoena20 <koenaletseku@gmail.com>
2024-08-13 05:47:07 +0000

    V

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,12 @@
-// Define and export function taskFirst using const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Define and export function getLast
 export function getLast() {
   return ' is okay';
 }
 
-// Define and export function taskNext using let
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
